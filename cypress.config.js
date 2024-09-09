3e4cc17f1507e9e435ed6768b12e8775714b724a
Carlos Miguel Tadifa <cdtadifa21@gmail.com>
2024-09-10 00:05:36 +0800

    new update for orangeHRM

---
+++ cypress.config.js
@@ -0,0 +1,45 @@
+const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   support: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+
+
+  module.exports = defineConfig({
+    // defaultCommandTimeout:15000,
+    // pageLoadTimeout: 30000,
+    // reporter: 'cypress-mochawesome-reported',
+    
+    e2e: {
+      // Define the folder where Cypress looks for test files
+      specPattern: [ 
+        'cypress/pageObjects', // This includes all spec files within cypress folder and its subfolders
+        'cypress/support' // This includes all spec files within cypress folder and its subfolders
+      ],
+      // You can also explicitly specify a different folder or pattern
+      // specPattern: 'cypress/tests/**/*.js',
+      baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', // Set the base URL if needed
+      
+      // Set Chrome as the default browser
+      browser: 'chrome',
+      // chromeWebSecurity: false
+    },
+  });
+
+
+
+
+    // module.exports = {
+    //   e2e: {
+    //     // Specify the pattern for test files
+    //     specPattern: [
+    //       'cypress/pageObjects/**/*.js', // Include all JS files in pageObjects folder
+    //       'cypress/support/TCM/TCMsaucedemo/Login/**/*.js' // Include all JS files in the specific support subfolder
+    //     ],
+    //     // Other options
+    //     baseUrl: 'http://your-app-url', // Set the base URL if needed
+    //   },
+    // };
